134b698165a5b49fac6660864d5105a8639520f7
leraaj <leraalfredojohn@gmail.com>
2025-05-12 22:04:41 +0800

    Added authorization

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.use(
     },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
